e7ba45b7238db169be4246e9f2ca2bae4cbfecfa
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-23 13:23:17 +0300

    feat: completed the goals page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
