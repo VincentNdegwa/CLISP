72b773da6abb3b33b9d191467130c72eb21f84ca
VincentNdegwa <ndegwavincent7@gmail.com>
2024-10-29 15:17:29 +0300

    payment

---
+++ app/Http/Controllers/PaymentsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentsController extends Controller
+{
+    public function createPayment($paymentDetails) {
+        
+    }
+}
