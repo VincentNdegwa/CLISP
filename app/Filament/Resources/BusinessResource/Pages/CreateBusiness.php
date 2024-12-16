978f450cf2f7b9d8482f70768c535401a80027b7
VincentNdegwa <ndegwavincent7@gmail.com>
2024-12-17 01:37:25 +0300

    business admin

---
+++ app/Filament/Resources/BusinessResource/Pages/CreateBusiness.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BusinessResource\Pages;
+
+use App\Filament\Resources\BusinessResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBusiness extends CreateRecord
+{
+    protected static string $resource = BusinessResource::class;
+}
